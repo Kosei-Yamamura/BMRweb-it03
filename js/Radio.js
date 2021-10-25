10e2dc1ffe7c459653f5bbf21f4e665615b8b2b3
山村昂世 <Kosei-Yamamura@users.noreply.github.com>
2021-10-26 01:59:55 +0900

    動画、ラジオの差し替え

--- js/Radio.js
+++ js/Radio.js
@@ -2,18 +2,17 @@
 
 
 // 要素
-const music = new Audio('https://drive.google.com/uc?id=1jXk5QG1PEqa8HLj4bg19tRFRtDwrI2io');
+const music = new Audio('https://drive.google.com/uc?id=1_OUqZalbX7eN4olu53_xXhln2nwLAbif');
 const play = document.getElementById('play');
 var img_play = document.getElementById('img_play');
 
-
 // 再生ボタン
-play.addEventListener('click', function(){
-    if(!music.paused){
-        img_play.src="img/play.jpg";
+play.addEventListener('click', function () {
+    if (!music.paused) {
+        img_play.src = "img/play.jpg";
         music.pause();
-    }else{
-        img_play.src="img/pause.jpg";
+    } else {
+        img_play.src = "img/pause.jpg";
         music.play();
     }
 });
